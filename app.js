7a707a42eb5f7e993868a7f472043a83c90b5253
Odunayo Joseph Babatunde <105566159+Freecodextra@users.noreply.github.com>
2022-10-21 13:32:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+//Get Username 
+var username = prompt("Welcome to Guessing Game. Enter a username");
+function start() {
+  var username = prompt("Welcome to Guessing Game. Enter a username");
+  random(1);
+ }
+ 
+  // GAME FUNCTION 
+  function random(level) {
+    //Get Number
+    var num = prompt("Choose a number between 1 and "+ (level+1));
+    //Get Random Number
+    var randNum = Math.round(Math.random() * (level + 1));
+    randNum = randNum === 0 ? 1 : randNum;
+    if(num == randNum) {
+      alert("Welcome to Level "+ (level+1));
+      random(level + 1);
+    } else if (num != randNum) {
+      alert("Game Over "+ username + " Your Final Level is "+ level);
+      var restart = window.confirm("Do You Want To Restart?");
+      if (restart) {
+        random(1);
+      }
+    }
+  }
+  random(1);
